d5e876d78b7f528e65f774814bd6d8c3bf098baa
Duy Thien <duythien0912@gmail.com>
2018-10-03 22:34:37 +0700

    #[thien] delete comment in server/index.js and fix readme.md

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import { ConnectDatabase } from './Databases/Connect'
-
 import categoriesRoutes from './routes/categoriesRoutes'
 import ordersRoutes from './routes/ordersRoutes'
 import productsRoutes from './routes/productsRoutes'
@@ -11,25 +10,12 @@ import usersRoutes from './routes/usersRoutes'
 
 const app = express();
 
- 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 ConnectDatabase();
 
 const port = process.env.PORT || 5000;
-
-// app.get("/api/flower", (req, res) => {
-//     const flowers = [
-//         { id: 1, ten: "hoa giang" },
-//         { id: 2, ten: "hoa giang1" },
-//         { id: 3, ten: "hoa giang2" }
-//     ];
-//     res.json(flowers);
-// });
 
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/orders', ordersRoutes);
