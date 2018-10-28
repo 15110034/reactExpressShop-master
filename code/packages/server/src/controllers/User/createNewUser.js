4dc10cbf46dda2afa82b5e5154fb3efb9cb5b8d0
thien <duythien0912@gmail.com>
2018-10-29 00:12:10 +0700

    † fix bug †

---
+++ code/packages/server/src/controllers/User/createNewUser.js
@@ -0,0 +1,35 @@
+import { isEmpty } from 'ramda';
+import { hash } from 'argon2';
+
+const UsersModel = require('../../models/usersModel.js');
+
+async function createNewUser(req, res) {
+  const { email = '', password = '' } = req.body;
+
+  if (isEmpty(req.body)) {
+    res.status(201).json({ code: 0, msg: 'Body empty' });
+    throw new Error('Body empty');
+  }
+  const hashedPassword = await hash(password);
+
+  const user = await UsersModel.findOne({ email }).catch(error => res.json({ code: 0, data: error, msg: 'Error when get user in database' }));
+  if (user) {
+    return res.json({ code: 0, msg: 'This Email is already taken. Try another Email' });
+  }
+
+  const users = new UsersModel({
+    email,
+    password: hashedPassword,
+  });
+  await users.save((err, usersData) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error when creating users',
+        error: err,
+      });
+    }
+    return res.status(201).json({ code: 1, msg: 'success', data: { email: usersData.email } });
+  });
+  return true;
+}
+export { createNewUser };
---
+++ code/packages/server/src/controllers/User/loginUser.js
@@ -0,0 +1,34 @@
+import { verify } from 'argon2';
+import jwt from 'jsonwebtoken';
+import { secretKey } from '../../config';
+
+const usersModel = require('../../models/usersModel.js');
+
+async function loginUser(req, res) {
+  const { email = '', password = '' } = req.body;
+  const user = await usersModel.findOne({ email }).catch((error) => {
+    res.json({ code: 0, msg: 'Error when get user in database' });
+    throw new Error(error);
+  });
+  if (!user) {
+    res.json({ code: 0, msg: 'Not found email' });
+    throw new Error('Not found email');
+  }
+  const valid = await verify(user.password, password);
+  if (!valid) {
+    res.json({ code: 0, msg: 'Error Password' });
+    throw new Error('Error Password');
+  }
+  const emailJwt = { email: user.email };
+  const token = jwt.sign(emailJwt, secretKey);
+  req.session.userId = user._id;
+  res.json({
+    code: 1,
+    data: { email: user.email },
+    token,
+    msg: 'Success',
+  });
+  return user;
+}
+
+export { loginUser };
