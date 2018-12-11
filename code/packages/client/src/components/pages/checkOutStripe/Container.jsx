cd6adea84469ad9199f3f31b1858c0aa16e82291
key46 <duythien0912@gmail.com>
2018-12-11 15:41:07 +0700

    add stripe
    add stripe

---
+++ code/packages/client/src/components/pages/checkOutStripe/Container.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import LeftColumn  from "./Collumn/LeftColumn/LeftColumn1";
+import  RightColumn  from "./Collumn/RightColumn/RightColumn";
+import Axios from "axios";
+
+class Container extends Component {
+  state = {
+  cart:{}
+  };
+  componentDidMount = async () => {
+    try {
+      
+      await this.getData();
+     //console.log(this.cart);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getData = async ()  => {
+    var cartData = await Axios.get(`/api/cart/`);
+    
+    this.setState({
+      cart : cartData.data
+    });
+  };
+
+  render() {
+    const {
+    cart
+    } = this.state;
+   
+
+    return (
+      <div className="container">
+        <div className="row">
+          <LeftColumn/>
+          <RightColumn
+          cart={cart}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Container;
